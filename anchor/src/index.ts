34e7ecf60f683099fd9ff21058791010a2e335b2
Irving N. Llamas Covarrubias <irvingnor@gmail.com>
2020-03-02 23:27:24 -0600

    Added necessary files for Anchor
    Anchor files

---
+++ anchor/src/index.ts
@@ -0,0 +1,58 @@
+import { GraphQLServer } from 'graphql-yoga'
+import { importSchema } from 'graphql-import'
+import { Prisma } from './generated/prisma'
+import { Context } from './utils'
+import { Keypair } from 'stellar-sdk'
+import { AES } from 'crypto-js'
+
+const resolvers = {
+  Query: {
+    user(_, { username }, context: Context, info) {
+      return context.db.query.user(
+        {
+          where: {
+            username
+          }
+        },
+        info
+      )
+    }
+  },
+  Mutation: {
+    signup(_, { username }, context: Context, info) {
+      const keypair = Keypair.random()
+
+      const configCryptoScret = 'StellarIsAwesome-But-Do-Not-Put-This-Value-In-Code'
+
+      const secret = AES.encrypt(
+          keypair.secret(),
+          configCryptoScret
+      ).toString()
+
+      const data = {
+        username,
+        stellarAccount: keypair.publicKey(),
+        stellarSeed: secret
+      }
+
+      return context.db.mutation.createUser(
+        { data },
+        info
+      )
+    },
+  },
+}
+
+const server = new GraphQLServer({
+  typeDefs: './src/schema.graphql',
+  resolvers,
+  context: req => ({
+    ...req,
+    db: new Prisma({
+      endpoint: 'https://us1.prisma.sh/public-gravelcloud-78/anchorx-api/dev', // the endpoint of the Prisma API
+      debug: true, // log all GraphQL queries & mutations sent to the Prisma API
+      // secret: 'mysecret123', // only needed if specified in `database/prisma.yml`
+    }),
+  }),
+})
+server.start(() => console.log('Server is running on http://localhost:4000'))
